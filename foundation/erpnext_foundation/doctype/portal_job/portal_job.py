e3482e20b4f11c79d35fd08b3f72cb53979cd892
Ashwini Save <ashwini@erpnext.com>
2017-05-24 14:43:55 +0530

    Changes for mail format

--- foundation/erpnext_foundation/doctype/portal_job/portal_job.py
+++ foundation/erpnext_foundation/doctype/portal_job/portal_job.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from frappe.utils import get_fullname, get_link_to_form
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
 import foundation
@@ -21,21 +22,32 @@ class PortalJob(WebsiteGenerator):
 			self.route = 'erpnext-jobs/' + self.scrub(self.title)
 
 	def on_update(self):
-		service_provider = frappe.get_all('Service Provider', fields=['name', 'email', 'route'])
-		recipients = [d.email for d in service_provider]
-		message = '<h2>'+ self.title + '</h2>'
-		message += '<table border=1 cellpadding=0 cellspacing=0 style="width: 350px; border-collapse: collapse;"><tr><td style="width: 100px; border: 1px solid #dadada; padding: 5px 10px;">Company Name</td><td style="border: 1px solid #dadada; padding: 5px 10px;">' + self.company_name + '</td></tr>'
-		message += '<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Country</td><td style="border: 1px solid #dadada; padding: 5px 10px;">' + self.country + '</td></tr>'
-		message += '<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Job Type</td><td style="border: 1px solid #dadada; padding: 5px 10px;">' + self.job_type + '</td></tr></table>'
+		service_providers = frappe.get_all('Service Provider', fields=['name', 'email', 'route'])
+		sender_fullname = get_fullname(frappe.session.user)
+		subject = _("New Job Posted by {0}").format(sender_fullname)
+		recipients = [d.email for d in service_providers]
+		message = _("""<h2> {0} </h2>"""\
+			.format(self.title))
+		message += _("""<table border=1 cellpadding=0 cellspacing=0 style="width: 350px; border-collapse: collapse;"><tr><td style="width: 100px; border: 1px solid #dadada; padding: 5px 10px;">Company Name</td><td style="border: 1px solid #dadada; padding: 5px 10px;"> {0} </td></tr>"""\
+			.format(self.company_name))
+		message += _("""<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Country</td><td style="border: 1px solid #dadada; padding: 5px 10px;"> {0}</td></tr>"""\
+			.format(self.country))
+		message += _("""<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Job Type</td><td style="border: 1px solid #dadada; padding: 5px 10px;">{0}</td></tr></table>"""\
+			.format(self.job_type))
 		message += '<h3>Details</h3>'
-		message += '<p>'+ self.description +'</p>'
-		message += '<table border=0 cellpadding=0 cellspacing=0 style="border-collapse: collapse; width: 350px;"><tr><td style="width: 100px; border: 1px solid #dadada; padding: 5px 10px;">Contact Name</td><td style="border: 1px solid #dadada; padding: 5px 10px;">' + frappe.session.user + '</td></tr>'
-		message += '<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Contact Number</td><td style="border: 1px solid #dadada; padding: 5px 10px;">' + self.contact_number + '</td></tr></table>'
-		message += '<p><a href="'+ self.route +'">View this Job Online</a></p>'
-		message += '<p style="font-style:italic;paddng-top:20px;">Note: You are getting this job notification, because you are a gold/silver/individual member in the ERPNext Foundation.</p>'
+		message += _("""<p>{0}</p>"""\
+			.format(self.description))
+		message += _("""<table border=0 cellpadding=0 cellspacing=0 style="border-collapse: collapse; width: 350px;"><tr><td style="width: 100px; border: 1px solid #dadada; padding: 5px 10px;">Contact Name</td><td style="border: 1px solid #dadada; padding: 5px 10px;">{0}</td></tr>"""\
+			.format(frappe.session.user))
+		message += _("""<tr><td style="border: 1px solid #dadada; padding: 5px 10px;">Contact Number</td><td style="border: 1px solid #dadada; padding: 5px 10px;">{0}</td></tr></table>"""\
+			.format(self.contact_number))
+		message += _("""<p><a href="{0}">View this Job Online</a></p>"""\
+			.format(self.route))
+		message += '<p style="font-style:italic;paddng-top:20px;">' + frappe._("Note: You are getting this job notification, because you are a gold/silver/individual member in the ERPNext Foundation.")+'</p>'
+		message +="<p>&nbsp;</p>"
 		frappe.sendmail(recipients = recipients,
 				message = message,
-				subject = "New job posted")
+				subject = subject)
 
 def get_list_context(context):
 	context.allow_guest = True
