a568336569bb6734227c65ba9976da13df7bfee8
Vishal <vishaldhayagude07@gmail.com>
2017-09-14 13:06:24 +0530

    [new] Fetch membership expiration details on member doctype

--- foundation/erpnext_foundation/doctype/member/member.js
+++ foundation/erpnext_foundation/doctype/member/member.js
@@ -3,6 +3,20 @@
 
 frappe.ui.form.on('Member', {
 	refresh: function(frm) {
+		frappe.call({
+			method:"frappe.client.get_value",
+			args:{
+				'doctype':"Membership",
+				'filters':{'member': frm.doc.name},
+				'fieldname':[
+					'to_date'
+				]
+			},
+			callback: function (data) {
+				frappe.model.set_value(frm.doctype,frm.docname, "expires_on", data.message.to_date);
+			}
+		});
+
 
 	}
 });
