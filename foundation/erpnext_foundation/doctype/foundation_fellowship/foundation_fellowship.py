a6111a2bd31348b0ed547ce45b0f83b072309b93
Vishal <vishaldhayagude07@gmail.com>
2017-10-27 16:00:46 +0530

    [new] Foundation Fellowship Doctype Added

--- foundation/erpnext_foundation/doctype/foundation_fellowship/foundation_fellowship.py
+++ foundation/erpnext_foundation/doctype/foundation_fellowship/foundation_fellowship.py
@@ -7,4 +7,23 @@ import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class FoundationFellowship(WebsiteGenerator):
-	pass
+    _website = frappe._dict(
+        condition_field = "show_in_website",
+    )
+    def validate(self):
+        if not self.route:
+            self.route = 'foundation-fellow/' + self.scrub(self.name)
+
+    def get_context(self, context):
+        context.no_cache = True
+        context.parents = [dict(label='View All Foundation Fellows',
+            route='foundation-fellow', title='View All foundation fellows')]
+
+
+
+def get_list_context(context):
+    context.allow_guest = True
+    context.no_cache = True
+    context.no_breadcrumbs = True
+    context.order_by = 'creation desc'
+    context.introduction = '<p>Following are the Foundation Fellows</p>'
