26468447379b882428689658a89f23bd4ff97fba
Vishal <vishaldhayagude07@gmail.com>
2017-11-01 16:55:00 +0530

    [new] Administrator can modify membership date details

--- foundation/erpnext_foundation/doctype/membership/membership.py
+++ foundation/erpnext_foundation/doctype/membership/membership.py
@@ -43,12 +43,15 @@ class Membership(Document):
 		# get last membership (if active)
 		last_membership = foundation.get_last_membership()
 
-		if last_membership:
+		# if person applied for offline membership
+		if last_membership and not frappe.session.user == "Administrator":
 			# if last membership does not expire in 30 days, then do not allow to renew
-			if getdate(add_days(last_membership.to_date, -30)) > getdate(nowdate()):
+			if getdate(add_days(last_membership.to_date, -30)) > getdate(nowdate()) :
 				frappe.throw(_('You can only renew if your membership expires within 30 days'))
 
 			self.from_date = add_days(last_membership.to_date, 1)
+		elif frappe.session.user == "Administrator":
+			self.from_date = self.from_date
 		else:
 			self.from_date = nowdate()
 
