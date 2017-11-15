bbc5c10cea0da6c18317562642bec567a1baa843
Vishal <vishaldhayagude07@gmail.com>
2017-11-15 15:58:34 +0530

    [new] On Chapter Leave member details not erased

--- foundation/www/join_chapter.py
+++ foundation/www/join_chapter.py
@@ -4,14 +4,14 @@ def get_context(context):
 	context.no_cache = True
 	chapter = frappe.get_doc('Chapter', frappe.form_dict.name)
 	if frappe.session.user!='Guest':
-		if frappe.session.user in [d.user for d in chapter.members]:
+		if frappe.session.user in [d.user for d in chapter.members if d.enabled == 1]:
 			context.already_member = True
 		else:
 			if frappe.request.method=='GET':
 				pass
 			elif frappe.request.method=='POST':
 				chapter.append('members', dict(
-					user=frappe.session.user, 
+					user=frappe.session.user,
 					introduction=frappe.form_dict.introduction,
 					website_url=frappe.form_dict.website_url,
 					enabled=1
--- foundation/www/leave_chapter.py
+++ foundation/www/leave_chapter.py
@@ -7,9 +7,23 @@ def get_context(context):
 	if frappe.session.user in [d.user for d in chapter.members]:
 		user = frappe.session.user
 		parent = frappe.form_dict.name
-		frappe.db.sql("""delete from `tabChapter Member` where parent = %s and user = %s """, (parent, user))
+		leave_reason = frappe.form_dict.leave
+		frappe.db.sql("""UPDATE `tabChapter Member` SET enabled = 0, leave_reason = %s WHERE parent = %s and user = %s ;""", (leave_reason, parent, user))
 		frappe.db.commit()
+
 
 	context.member_deleted = True
 
 	context.chapter = chapter
+
+# def leave(title, user_id, leave_reason):
+# 	chapter = frappe.get_doc("Chapter", title)
+# 	for member in chapter.members:
+# 		for x in xrange(1,10):
+# 			print(member.user, user_id)
+# 		if member.user == user_id:
+# 			member.enabled = 0
+# 			member.leave_reason = leave_reason
+# 	chapter.save()
+# 	frappe.db.commit()
+# 	return "Thank you for Feedback"
