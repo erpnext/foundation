d47b4ab6445fdc8c913b08a12edc54d83d69a39b
Basawaraj <asbasawaraj@gmail.com>
2018-05-12 21:59:36 +0530

    membership history display

--- foundation/www/members/details.py
+++ foundation/www/members/details.py
@@ -7,3 +7,4 @@ def get_context(context):
 	if frappe.session.user != 'Guest':
 		context.show_sidebar = True
 		context.last_membership = foundation.get_last_membership()
+		context.all_memberships = foundation.get_all_memberships_of_one_member()
