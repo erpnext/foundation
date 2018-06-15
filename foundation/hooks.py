789dc00b416cf6e52643084dedbdb8e69d101a65
Rushabh Mehta <rmehta@gmail.com>
2018-06-15 16:40:15 +0530

    medium style

--- foundation/hooks.py
+++ foundation/hooks.py
@@ -39,7 +39,7 @@ home_page = "index"
 template_apps = ['foundation', 'frappe_theme', 'erpnext', 'frappe']
 website_context = {
 	# "navbar_search": 1,
-	"brand_html": "<img class='mr-2 d-inline-block align-top' width='30px' height='30px' src='/assets/foundation/img/erpnext-logo-white.svg' alt='Oensource ERP Software'/>ERPNext.org",
+	"brand_html": "<img class='mr-2 d-inline-block align-top' width='30px' height='30px' src='/assets/foundation/img/erpnext-logo-black.svg' alt='Opensource ERP Software'/>ERPNext.org",
 	"top_bar_items": [
 			{"label": "ERPNext", "child_items":[
 				{"label": "User Stories", "url": "/stories"},
