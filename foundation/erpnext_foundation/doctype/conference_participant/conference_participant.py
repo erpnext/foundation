b734ee469b7df30004efa010c19193907f245c5c
Zlash65 <zarrar65@gmail.com>
2018-10-02 12:37:50 +0530

    add field to fetch email for participants

--- foundation/erpnext_foundation/doctype/conference_participant/conference_participant.py
+++ foundation/erpnext_foundation/doctype/conference_participant/conference_participant.py
@@ -9,9 +9,12 @@ from frappe.model.document import Document
 class ConferenceParticipant(Document):
 	def validate(self):
 		self.conference = '2018'
+		if self.owner not in ["Guest", "Administrator"] and not self.get("email") \
+			and frappe.db.get_value("User", self.owner, "user_type") == "Website User":
+			self.email = frappe.db.get_value('User', self.owner, "email")
 
 	def validate_payment(self):
-		self.amount = self.full_conference_tickets * (4000 if self.currency=='INR' else 60)
+		self.amount = self.full_conference_tickets * (4000 if self.currency == 'INR' else 60)
 
 	def on_payment_authorized(self, status_changed_to=None):
 		self.paid = 1
