6f485b4871c5a8a2f5d32223574e3477983e5404
Kenneth Sequeira <kenneth@Kenneths-MacBook-Air.local>
2018-10-10 17:14:01 +0530

    autoname added for Dev Sprint doctype

--- foundation/erpnext_foundation/doctype/developer_sprint/developer_sprint.py
+++ foundation/erpnext_foundation/doctype/developer_sprint/developer_sprint.py
@@ -5,6 +5,10 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 class DeveloperSprint(Document):
-	pass
+	def autoname(self):
+		autoname = 'DEV-SPRINT-.YYYY.-.###'
+		self.name = make_autoname(autoname)
+
