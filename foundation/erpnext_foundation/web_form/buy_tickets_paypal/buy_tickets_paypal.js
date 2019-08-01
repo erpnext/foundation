e1ee24b44f7eca24fb5b2dd81a6eedc3f1348624
Shivam Mishra <scm.mymail@gmail.com>
2019-08-01 16:15:34 +0530

    fix: updated scripts with new webform API

--- foundation/erpnext_foundation/web_form/buy_tickets_paypal/buy_tickets_paypal.js
+++ foundation/erpnext_foundation/web_form/buy_tickets_paypal/buy_tickets_paypal.js
@@ -1,8 +1,8 @@
 frappe.ready(function() {
 	// bind events here
 	frappe._calculate = function() {
-		frappe.web_form.field_group.set_value('amount',
-			parseInt(frappe.web_form.field_group.get_value('full_conference_tickets') || 0) * 60
+		frappe.web_form.set_value('amount',
+			parseInt(frappe.web_form.get_value('full_conference_tickets') || 0) * 60
 		)
 	};
 
